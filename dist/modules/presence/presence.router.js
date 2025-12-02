36b913e7d8437e7dbd8de112189e828e0721b93e
Stephen Regan James Bacolod Layson <stephen@kooya.ph>
2025-12-02 23:51:21 +0800

    dev init

--- dist/modules/presence/presence.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.presenceRouter = void 0;
-const express_1 = require("express");
-const authenticate_1 = require("../../middleware/authenticate");
-const presence_controller_1 = require("./presence.controller");
-exports.presenceRouter = (0, express_1.Router)();
-exports.presenceRouter.get('/', authenticate_1.authenticate, presence_controller_1.getPresenceSnapshot);
